601bd0d3a5e9d05d5cf929aa21d44923edde12d2
Datunashvili Giorgi <datunashvili_giorgi@yahoo.com>
2020-11-17 21:50:42 +0400

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatunashviliAPI.Data;
+using DatunashviliAPI.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatunashviliAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await context.Products.FindAsync(id);
+        }
+
+
+
+        [HttpGet("getSingleString")]
+        public string GetProductString()
+        {
+           return "ეს არი პროდუქციის სია.";
+        }
+
+        [HttpGet("getSingleString/{id}")]
+        public string GetProduct(int id)
+        {
+            return "ერთი პროდუქტი";
+        }
+    }
+}
---
+++ Data/StoreContext.cs
@@ -0,0 +1,18 @@
+﻿using DatunashviliAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatunashviliAPI.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
